9c5408f7b4221990ed7b5bba25f9c55b3f195c60
U10416011 <u10416011@go.utaipei.edu.tw>
2016-04-24 11:46:02 +0800

    create TreeNode
    TreeNode for the node of the tree

---
+++ BST.java
@@ -0,0 +1,8 @@
+//U10416011
+public class BST {
+	public TreeNode root;
+	public BST(Object x) { //constructor
+		root = new TreeNode(x);
+	}
+
+}
---
+++ TreeNode.java
@@ -0,0 +1,18 @@
+
+public class TreeNode {//node for tree
+	public Object element;
+	public TreeNode left;
+	public TreeNode right;
+	//default constructor
+	public TreeNode(){ 
+	}
+	// constructor 
+	public TreeNode(Object theElement) {
+		this(theElement, null, null);
+	}
+	public TreeNode(Object theElement, TreeNode lLink, TreeNode rLink) {
+		element = theElement;
+		this.left = lLink;
+		this.right = rLink;
+	}
+}
